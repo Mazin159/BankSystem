9beeae3401a1f23a5e10a844b0be70d6ae95fc3f
MazinMoh <mazinmoh@gmail.com>
2026-04-09 19:41:07 +0200

    Add Split String Function

--- BankSystem/BankSystem.cpp
+++ BankSystem/BankSystem.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
 using namespace std;
 
 struct sClient {
@@ -11,9 +13,33 @@ struct sClient {
 	bool MarkToDelete = false;
 };
 
+vector<string> SplitString(string s1, string delim) {
+	string Word;
+	vector<string> vString;
+	int pos = 0;
+	while ((pos = s1.find(delim)) != std::string::npos) {
+
+		Word = s1.substr(0,pos);
+
+		if (Word != "") {
+		vString.push_back(Word);
+
+		}
+		s1.erase(0, pos + delim.length());
+	}
+
+	if (s1 != "") {
+		vString.push_back(s1);
+	}
+
+	return vString;
+}
+
 
 int main()
 {
+	
+	
 	
 }
 
