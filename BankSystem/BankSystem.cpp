3866ef405811cd073455489cda1ea7a87896976c
MazinMoh <mazinmoh@gmail.com>
2026-04-09 18:39:43 +0200

    Add struct (sClient)

--- BankSystem/BankSystem.cpp
+++ BankSystem/BankSystem.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+struct sClient {
+	string AccountNumber;
+	string Name;
+	string PinCode;
+	string Number;
+	double Salary;
+	bool MarkToDelete = false;
+};
 
 
 int main()
 {
-
+	
 }
 
